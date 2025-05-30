07fb713dbd92dac78d63e89912a9a0ca31f87537
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2025-05-30 16:27:17 +0200

    correction_pocketbase

---
+++ backend/testback.js
@@ -0,0 +1,25 @@
+import {getUserbyPost, getPost, getUser, getUsers} from './backend.mjs';
+
+// try {const records = await getUserbyPost ('w83ir4g9w87ifl6') ;
+//     console.log(JSON.stringify(records, null, 2)) ;
+//     } catch (e) {
+//         console.error(e);
+//     };
+
+// try {const records = await getPost ('w83ir4g9w87ifl6') ;
+//     console.log(JSON.stringify(records, null, 2)) ;
+//     } catch (e) {
+//         console.error(e);
+//     };
+
+// try {const records = await getUser ('48x72a2dy8hcp35') ;
+//     console.log(JSON.stringify(records, null, 2)) ;
+//     } catch (e) {
+//         console.error(e);
+//     };
+
+// try {const records = await getUsers () ;
+//     console.log(JSON.stringify(records, null, 2)) ;
+//     } catch (e) {
+//         console.error(e);
+//     };
